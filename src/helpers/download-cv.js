deac063ac097f86caede4b03a3525e230350315b
Lucas Wendell <lucas.wendell.98@gmail.com>
2025-12-30 16:04:28 -0300

    feat: added download cv function

---
+++ src/helpers/download-cv.js
@@ -0,0 +1,5 @@
+const viewBtn = document.getElementById("downloadBtn");
+
+viewBtn.addEventListener("click", function() {
+    window.open("src/assets/documents/curriculo-lucas-wendell.pdf", "_blank");
+});
